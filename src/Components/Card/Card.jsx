d168360e1136e8f79e57dee1a799859cced98c60
Vitor Cesarino Marchese <vitorcesarino1@gmail.com>
2024-08-16 16:53:43 -0300

    change cards

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,14 +1,14 @@
 function Card(prop) {
   return (
     <>
-      <div className={prop.color}>
+      <div style={{background: `${prop.color}`}}>
         <div>
           <h2>{prop.id}</h2>
-          <img src={prop.img} alt="" />
+          <img src={prop.img} alt={prop.alt} />
         </div>
         <div>
-          <h3>prop.title</h3>
-          <p>prop.desc</p>
+          <h3>{prop.title}</h3>
+          <p>{prop.desc}</p>
         </div>
       </div>
     </>
